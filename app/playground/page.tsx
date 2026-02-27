24f78682872d9008109e42a37be1cf40023cd146
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2026-02-27 16:51:51 +0530

    component playground added

---
+++ app/playground/page.tsx
@@ -0,0 +1,13 @@
+function page() {
+  return (
+    <div className="flex items-center justify-center gap-5 p-10">
+        <div className="h-fit w-full border-2 border-black border-dashed p-3 rounded-md flex-col">
+            <h1 className="text-3xl font-bold">Buttons</h1>
+            <h2 className="mt-3 font-semibold">Basic Tailwind Buttons</h2>
+            
+        </div>
+    </div>
+  )
+}
+
+export default page
