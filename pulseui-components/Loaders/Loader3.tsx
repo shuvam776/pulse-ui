0c95aded4b8f16ef3078af961ec2c7af2041d500
Ayantik Sarkar <99005057+ayantik2006@users.noreply.github.com>
2026-02-28 19:19:54 +0530

    loader 3 added

--- pulseui-components/Loaders/Loader3.tsx
+++ pulseui-components/Loaders/Loader3.tsx
@@ -4,25 +4,21 @@ function Loader3() {
   return (
     <div className="flex items-center gap-2">
       <motion.div
-        animate={{ height: ["2.5rem", "1rem", "2.5rem"] }}
-        transition={{ duration: 1, repeat: Infinity, ease: "easeInOut" }}
-        className="bg-neutral-700 w-[5px] h-10 rounded-t-lg rounded-b-lg"
-      ></motion.div>
+        animate={{height:["2.5rem","1rem","2.5rem"]}} 
+        transition={{duration:1, repeat:Infinity, ease:"easeInOut"}}
+        className="bg-neutral-700 w-[5px] h-10 rounded-t-lg rounded-b-lg"></motion.div>
       <motion.div
-        animate={{ height: ["2.5rem", "1rem", "2.5rem"] }}
-        transition={{ duration: 1, repeat: Infinity, ease: "easeInOut" }}
-        className="bg-neutral-700 w-[5px] h-10 rounded-t-lg rounded-b-lg"
-      ></motion.div>
+        animate={{height:["1rem","2.5rem","1rem"]}} 
+        transition={{duration:1, repeat:Infinity, ease:"easeInOut"}}
+        className="bg-neutral-700 w-[5px] h-10 rounded-t-lg rounded-b-lg"></motion.div>
       <motion.div
-        animate={{ height: ["2.5rem", "1rem", "2.5rem"] }}
-        transition={{ duration: 1, repeat: Infinity, ease: "easeInOut" }}
-        className="bg-neutral-700 w-[5px] h-10 rounded-t-lg rounded-b-lg"
-      ></motion.div>
+        animate={{height:["2.5rem","1rem","2.5rem"]}} 
+        transition={{duration:1, repeat:Infinity, ease:"easeInOut"}}
+        className="bg-neutral-700 w-[5px] h-10 rounded-t-lg rounded-b-lg"></motion.div>
       <motion.div
-        animate={{ height: ["2.5rem", "1rem", "2.5rem"] }}
-        transition={{ duration: 1, repeat: Infinity, ease: "easeInOut" }}
-        className="bg-neutral-700 w-[5px] h-10 rounded-t-lg rounded-b-lg"
-      ></motion.div>
+        animate={{height:["1rem","2.5rem","1rem"]}} 
+        transition={{duration:1, repeat:Infinity, ease:"easeInOut"}}
+        className="bg-neutral-700 w-[5px] h-10 rounded-t-lg rounded-b-lg"></motion.div>
     </div>
   );
 }
